fa879a8c7455ed979d6dc0a5520b82c5c9c812cc
nilesh20gupta <nilesh20gupta@gmail.com>
2025-03-07 14:47:11 +0530

    ammend the git commit with second commit where file added along with git

--- locatGitView.java
+++ locatGitView.java
@@ -4,6 +4,7 @@ public class locatGitView {
 
 	public static void main(String[] args) {
 		System.out.println("Hi all.. my first git in local system");
+		System.out.println("Second git commit");
 
 	}
 
