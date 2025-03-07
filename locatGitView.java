48248409c69501d7f0301427e173c421e3978e08
nilesh20gupta <nilesh20gupta@gmail.com>
2025-03-07 20:18:27 +0530

    checking the git diff

--- locatGitView.java
+++ locatGitView.java
@@ -5,6 +5,7 @@ public class locatGitView {
 	public static void main(String[] args) {
 		System.out.println("Hi all.. my first git in local system");
 		System.out.println("Second git commit");
+		System.out.println("checking the git diff");
 
 	}
 
